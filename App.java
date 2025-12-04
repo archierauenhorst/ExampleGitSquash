70f46918a7c66320412416b599e4c10ff58fda8d
archierauenhorst <archierauenhorst@u.boisestate.edu>
2025-12-03 17:02:19 -0700

    Revision 2 in branch ([WIP] Start the implementation of task #123)

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 public class App{
     public void sampleMethod(){
         System.out.println("We'll assume that this change implements task #117.");
+        System.out.println("We'll assume that this change fixes bug #128");
+        System.out.println("Work in progress...");
+        System.out.println("Starting implementation of task #123.");
     }
 }
