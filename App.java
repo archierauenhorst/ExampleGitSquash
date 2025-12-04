8c0161eb692a39ca1d7d84c417beab825fc12762
archierauenhorst <archierauenhorst@u.boisestate.edu>
2025-12-03 17:02:57 -0700

    Revision 3 in branch (More progress with #123)

--- App.java
+++ App.java
@@ -4,5 +4,16 @@ public class App{
         System.out.println("We'll assume that this change fixes bug #128");
         System.out.println("Work in progress...");
         System.out.println("Starting implementation of task #123.");
+        //added new unit tests
+        //fixed bugs discovered during pair programming
+        //fixed static analysis warnings/errors
+        //refactored code
+        //fixed code formatting issues
+        //etc.
+        System.out.println("More progress towards implementing task #123");
+        /*added more unstable code...*/
+                /*added code that does not follow the team style guides...*/
+            /*added System.out.println debug statements...*/
+        /*the text in these block comments will have to be removed in future commits*/
     }
 }
